50ba38d046827915f304ce33db58e1f54eb66bfa
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-05-30 17:07:36 -0300

    Se agrega el navbar del proyecto

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,23 +1,20 @@
-import { Nav } from "react-bootstrap";
+import { Nav, Navbar, Container } from "react-bootstrap";
 
 const Menu = () => {
   return (
-      <Nav className="justify-content-end menu" activeKey="/home">
-        <Nav.Item>
-          <Nav.Link href="/home">Active</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="disabled" disabled>
-            Disabled
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Cafecito</Navbar.Brand>
+          <Nav className="me-0">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#features">Registro</Nav.Link>
+            <Nav.Link href="#pricing">Administrador</Nav.Link>
+            <Nav.Link href="#pricing">Login</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
