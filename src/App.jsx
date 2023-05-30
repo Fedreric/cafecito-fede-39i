f14658b79e305155c9a9fb81e8a1ebfbd3f80b16
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-05-30 17:00:08 -0300

    Se agrega el componente Menu.jsx en una etiqueta header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';  
 import './App.css'
+import Menu from './components/common/Menu';
 
 function App() {
 
   return (
     <>
-      
+      <header>
+        <Menu></Menu>
+      </header>
     </>
   )
 }
