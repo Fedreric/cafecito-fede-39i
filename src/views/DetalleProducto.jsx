eb5d27a8f20d963a5048f9f32a08f39880a3db14
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-05-30 19:06:12 -0300

    Se crea el componente DetalleProducto.jsx y se le da su maquetado basico

--- src/views/DetalleProducto.jsx
+++ src/views/DetalleProducto.jsx
@@ -1,23 +1,20 @@
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Image } from "react-bootstrap";
 
 const DetalleProducto = () => {
   return (
-    <div>
+    <Container className="my-5">
       <Row xs={1} md={2}>
-        <Container>
-          <Card.Img
-            variant="top"
-            src="https://cdn7.kiwilimon.com/recetaimagen/36986/640x640/46349.jpg.webp"
-          />
+        <Container className="mb-4">
+        <Image src="https://cdn7.kiwilimon.com/recetaimagen/36986/640x640/46349.jpg.webp" fluid className="ounded" />
         </Container>
         <Container>
-            <h2>Nombre del producto</h2>
+            <h2 className="display-6 text-center">Nombre del producto</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos eum nemo similique quo non quam dolor nulla! Molestiae iusto error quod alias nulla velit adipisci quidem. Obcaecati facere sapiente doloribus? Nulla pariatur, autem tenetur alias ea delectus vel praesentium excepturi.</p>
-            <p><span>Categoria:</span> Café</p> 
-            <p><span>Precio:</span> $900.00</p> 
+            <p><b className="text-danger">Categoria:</b> Café</p> 
+            <p><b className="text-danger">Precio:</b> $900.00</p> 
         </Container>
       </Row>
-    </div>
+    </Container>
   );
 };
 
