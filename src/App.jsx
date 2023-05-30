ea2bf26af03396c43e81509e8bde955aa44bad1a
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-05-30 17:52:41 -0300

    Se agrega y realiza el componente Inicio.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Menu from './components/common/Menu';
 import FooterFedeLedesma from './components/common/FooterFedeLedesma';
+import Inicio from './views/Inicio';
 
 function App() {
 
   return (
-    < >
+    <>
       <section className='mainPage'>
         <header>
         <Menu></Menu>
         </header>
+        <Inicio></Inicio>
       </section>
       <FooterFedeLedesma></FooterFedeLedesma>
     </>
---
+++ src/views/Inicio.jsx
@@ -0,0 +1,16 @@
+import {Image, Container, Row} from 'react-bootstrap';
+const Inicio = () => {
+    return (
+        <>
+        <Image src="https://media.glamour.mx/photos/6190b72fa6e030d64810483c/master/w_1600%2Cc_limit/225340.jpg" fluid />
+        <Container>
+            <h1 className='text-center display-4 my-5'>Nuestros Productos</h1>
+            <Row>
+                
+            </Row>
+        </Container>
+        </>
+    );
+};
+
+export default Inicio;
