056ce9982770cfee4eda3dc44d7bb4eea5a059d0
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-05-30 17:34:24 -0300

    Se agrega el boton hamburguesa al navbar

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,17 +1,20 @@
-import { Nav, Navbar, Container } from "react-bootstrap";
+import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 
 const Menu = () => {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Cafecito</Navbar.Brand>
-          <Nav className="me-0">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#features">Registro</Nav.Link>
-            <Nav.Link href="#pricing">Administrador</Nav.Link>
-            <Nav.Link href="#pricing">Login</Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+            <Nav className="text-center">
+              <Nav.Link href="#home">Inicio</Nav.Link>
+              <Nav.Link href="#features">Registro</Nav.Link>
+              <Nav.Link href="#pricing">Administrador</Nav.Link>
+              <Nav.Link href="#pricing">Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
