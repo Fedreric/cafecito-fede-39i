e9d71e3f145b4d92d7d163106f07e8ffe952341e
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-05 10:20:34 -0300

    Vinculamos los items del nav con las rutas

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,18 +1,19 @@
 import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
 
 const Menu = () => {
   return (
     <>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Cafecito</Navbar.Brand>
+          <Navbar.Brand as={Link} to={'/'}>Cafecito</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
             <Nav className="text-center">
-              <Nav.Link href="#home">Inicio</Nav.Link>
-              <Nav.Link href="#features">Registro</Nav.Link>
-              <Nav.Link href="#pricing">Administrador</Nav.Link>
-              <Nav.Link href="#pricing">Login</Nav.Link>
+              <NavLink end className={'nav-item nav-link'} to={'/'}>Inicio</NavLink>
+              <NavLink end className={'nav-item nav-link'} to={'/registro'}>Registro</NavLink>
+              <NavLink end className={'nav-item nav-link'} to={'/administrador'}>Administrador</NavLink>
+              <NavLink end className={'nav-item nav-link'} to={'/login'}>Login</NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
