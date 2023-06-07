bdfbaf603090e96aee0fd384714615444256fd5f
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-06 21:45:29 -0300

    Se agrega validacion de required al formulario

--- src/components/views/Registro.jsx
+++ src/components/views/Registro.jsx
@@ -1,37 +1,65 @@
 import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const Registro = () => {
-    return (
-        <div className="mt-5 mainSection">
-        <h3 className="text-center">Registro</h3>
-        <div className="row justify-content-center">
-          <div className="col-12 col-sm-8 col-md-6 col-xl-4">
-            <Form>
-              <Form.Group className="mb-2">
-                <Form.Control
-                  type="text"
-                  placeholder="Ingrese un nombre de usuario"
-                />
-              </Form.Group>
-              <Form.Group className="mb-2">
-                <Form.Control placeholder="Ingrese un email" />
-              </Form.Group>
-              <Form.Group className="mb-2">
-                <Form.Control type="password" placeholder="Ingrese un password" />
-              </Form.Group>
-              <div className="row">
-                <Button
-                  className="btn btn-dark btn-lg btn-block mb-2"
-                  type="submit"
-                >
-                  Registrar
-                </Button>
-              </div>
-            </Form>
-          </div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const onSubmit = (usuario) => {
+    console.log(usuario);
+  };
+
+  return (
+    <div className="mt-5 mainSection">
+      <h3 className="text-center">Registro</h3>
+      <div className="row justify-content-center">
+        <div className="col-12 col-sm-8 col-md-6 col-xl-4">
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group className="mb-2">
+              <Form.Control
+                type="text"
+                placeholder="Ingrese un nombre de usuario"
+                {...register("usuario", {
+                  required: "Ingrese el nombre de usuario",
+                })}
+              />
+              <Form.Text className="text-danger fst-italic">
+                {errors.usuario?.message}
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-2">
+              <Form.Control placeholder="Ingrese un email" {...register("email", {
+                  required: "Ingrese el email",
+                })}/>
+                <Form.Text className="text-danger fst-italic">
+                {errors.email?.message}
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-2">
+              <Form.Control type="password" placeholder="Ingrese un password" {...register("password", {
+                  required: "Ingrese la password",
+                })}/>
+                <Form.Text className="text-danger fst-italic">
+                {errors.password?.message}
+              </Form.Text>
+            </Form.Group>
+            <div className="row">
+              <Button
+                className="btn btn-dark btn-lg btn-block mb-2"
+                type="submit"
+              >
+                Registrar
+              </Button>
+            </div>
+          </Form>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Registro;
