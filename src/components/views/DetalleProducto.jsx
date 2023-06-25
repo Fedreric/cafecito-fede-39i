9cb7b629af1eb01bf5512409d5b11a5cbaf6abf5
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-25 12:34:53 -0300

    Se realiza el read del detalle del producto

--- src/components/views/DetalleProducto.jsx
+++ src/components/views/DetalleProducto.jsx
@@ -1,17 +1,26 @@
+import { useEffect, useState } from "react";
 import { Container, Row, Image } from "react-bootstrap";
-
+import { useParams } from "react-router-dom";
+import { obtenerProducto } from "../helpers/queries";
 const DetalleProducto = () => {
+  const {id} = useParams();
+  const [producto, setProducto] = useState({})
+  useEffect(()=>{
+    obtenerProducto(id).then((respuesta)=>{
+      setProducto(respuesta);
+    })
+  },[])
   return (
     <Container className="my-5">
       <Row xs={1} md={2}>
         <Container className="mb-4">
-        <Image src="https://cdn7.kiwilimon.com/recetaimagen/36986/640x640/46349.jpg.webp" fluid className="ounded" />
+        <Image src={producto.imagen} fluid className="ounded" />
         </Container>
         <Container>
-            <h2 className="display-6 text-center">Nombre del producto</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos eum nemo similique quo non quam dolor nulla! Molestiae iusto error quod alias nulla velit adipisci quidem. Obcaecati facere sapiente doloribus? Nulla pariatur, autem tenetur alias ea delectus vel praesentium excepturi.</p>
-            <p><b className="text-danger">Categoria:</b> Café</p> 
-            <p><b className="text-danger">Precio:</b> $900.00</p> 
+            <h2 className="display-6 text-center">{producto.nombreProducto}</h2>
+            <p>{producto.descripcion}</p>
+            <p><b className="text-danger">Categoria:</b> {producto.categoria}</p> 
+            <p><b className="text-danger">Precio:</b> ${producto.precio}</p> 
         </Container>
       </Row>
     </Container>
