426824a16e6a27bb3bf6014a532f2261871500f1
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-25 12:26:55 -0300

    Se hace uso de la funcion obtenerProductos y se manda cada producto a CardProducto como props

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,22 +1,34 @@
-import {Image, Container, Row} from 'react-bootstrap';
-import CardProducto from './producto/CardProducto';
+import { Image, Container, Row } from "react-bootstrap";
+import CardProducto from "./producto/CardProducto";
+import { useState, useEffect } from "react";
+import { obtenerProductos } from "../helpers/queries";
 const Inicio = () => {
-    return (
-        <>
-        <Image src="https://media.glamour.mx/photos/6190b72fa6e030d64810483c/master/w_1600%2Cc_limit/225340.jpg" fluid />
-        <Container className='mb-5'>
-            <h1 className='text-center display-4 my-5'>Nuestros Productos</h1>
-            <Row xs={1} md={2} lg={3}>
-                <CardProducto></CardProducto>
-                <CardProducto></CardProducto>
-                <CardProducto></CardProducto>
-                <CardProducto></CardProducto>
-                <CardProducto></CardProducto>
-                <CardProducto></CardProducto>
-            </Row>
-        </Container>
-        </>
-    );
+  const [productos, setProductos] = useState([]);
+  useEffect(() => {
+    obtenerProductos().then((respuesta) => {
+      console.log(respuesta);
+      setProductos(respuesta);
+      //resolver que sucede al no iniciar la api
+    });
+  }, []);
+  return (
+    <>
+      <Image
+        src="https://media.glamour.mx/photos/6190b72fa6e030d64810483c/master/w_1600%2Cc_limit/225340.jpg"
+        fluid
+      />
+      <Container className="mb-5">
+        <h1 className="text-center display-4 my-5">Nuestros Productos</h1>
+        <Row xs={1} md={2} lg={3}>
+            {
+             productos.map((producto)=>{
+                return <CardProducto producto={producto}></CardProducto> 
+             })
+            }
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default Inicio;
